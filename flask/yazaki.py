b091288d2f14da76687b78660ebb33ff53aed0ba
brywhitak <brywhit@bgsu.edu>
2023-10-22 02:22:04 -0400

    Add convert_to_abbreviations function to be used with mapbox regions

--- flask/yazaki.py
+++ flask/yazaki.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 
+
 app = Flask(__name__)
 
 FILE_NAME = "../HackDearborn_Yazaki_MfgInfo_SECRET.xlsx"
@@ -36,5 +37,21 @@ def preprocess_data(data_sheet):
             country_name_dict.update({n: 0})
         country_name_dict[n] += 1
     print(country_name_dict)
+    test = data_sheet['StateProvince'].str.lower().values.tolist()
+    print(convert_abbreviations(test))
     return data_sheet
+
+def convert_abbreviations(array):
+    ABBREVIATIONS = {'chihuahua' : 'MX-CHH', 'chi' : 'MX-CHH', 'durango' : 'MX-DUR', 'dgo' : 'MX-DUR', 'nuevo leon' : 'MX-NLE', 
+                     'chiapas' : 'MX-CHP', 'chs' : 'MX-CHP', 'sonora' : 'MX-SON', 'son' : 'MX-SON', 'san luis potosi' : 'MX-SLP', 
+                     'jalisco' : 'MX-JAL', 'coahuila' : 'MX-COA', 'coa' : 'MX-COA', 'colima' : 'MX-COL', 'col' : 'MX-COL', 
+                     'gto' : 'MX-GUA', 'leon' : 'NI', 'mi' : 'US-MI', 'tn' : 'US-TN', 'santa ana' : 'US-CA', 'san marcos' : 'GT',
+                     'mx' : 'MX'}
+    for i in range(len(array)):
+        if type(array[i]) != str:
+            array[i] = 'leon'
+        array[i] = array[i].lower()
+        if array[i] in ABBREVIATIONS:
+            array[i] = ABBREVIATIONS[array[i]]
+    return array
     

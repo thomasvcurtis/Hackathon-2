c082c9e6c7c3a43faaf55e53eff138731f3c340a
brywhitak <brywhit@bgsu.edu>
2023-10-22 11:25:22 -0400

    Update map.js and map.html

--- flask/static/map.js
+++ flask/static/map.js
@@ -5,7 +5,7 @@ function CreateMap() {
   const map = new mapboxgl.Map({
     container: "map", // container ID
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-    style: "mapbox://styles/mapbox/streets-v12", // style URL
+    style: "mapbox://styles/crlilly/clo1lfhpj00ba01oyhvtt20xp",
     center: [-74.5, 40], // starting position [lng, lat]
     zoom: 9, // starting zoom
     projection: "mercator",
